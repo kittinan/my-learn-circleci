bfc9006968334428c54b8f794e38710a5e239ceb
Kittinan Srithaworn <tunxedge@gmail.com>
2017-10-15 11:12:07 +0700

    :tada: my first circle ci

---
+++ src/kittinan/util.py
@@ -0,0 +1,9 @@
+
+def add_number(x, y):
+    return x + y
+
+def minus_number(x, y):
+    return x - y
+
+def hahaha_number():
+    return 555
---
+++ tests/test_util.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append('./src')
+
+import unittest
+from kittinan import util
+
+class TestUtil(unittest.TestCase):
+
+    def test_add(self):
+        x = 1337
+        y = 1337
+        result = util.add_number(x, y)
+        expected = x + y
+        self.assertEqual(result, expected)
+
+    def test_minus(self):
+        x = 1337
+        y = 124
+        result = util.minus_number(x, y)
+        expected = x - y
+        self.assertEqual(result, expected)
+
+    def test_hahaha(self):
+        result = util.hahaha_number()
+        expected = 555
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
